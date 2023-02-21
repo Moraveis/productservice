a681af09e5c2b6ae6933f4eb2c794a238fdfe9bb
Joao Vitor <joaovitormoraveis@gmail.com>
2023-02-20 20:36:50 -0500

    Implementing command to create product

--- src/main/java/com/joao/studycase/productservice/controller/ProductController.java
+++ src/main/java/com/joao/studycase/productservice/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.joao.studycase.productservice.controller;
 
+import com.joao.studycase.productservice.command.CreateProductCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -24,6 +27,14 @@ public class ProductController {
 
     @PostMapping
     public String createProduct(@RequestBody ProductRequest request) {
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(request.getPrice())
+                .quantity(request.getQuantity())
+                .title(request.getTitle())
+                .productId(UUID.randomUUID().toString())
+                .build();
+
         return "HTTP POST Request handled. Product = " + request.getTitle();
     }
 
