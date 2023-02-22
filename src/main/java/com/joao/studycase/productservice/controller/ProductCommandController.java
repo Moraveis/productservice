04f769a0ff62ee862cea3e7b34340f8fc93a5603
Joao Vitor <joaovitormoraveis@gmail.com>
2023-02-21 20:47:15 -0500

    Adding validation for post request

--- src/main/java/com/joao/studycase/productservice/controller/ProductCommandController.java
+++ src/main/java/com/joao/studycase/productservice/controller/ProductCommandController.java
@@ -4,13 +4,13 @@ import com.joao.studycase.productservice.command.CreateProductCommand;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -26,7 +26,7 @@ public class ProductCommandController {
     }
 
     @PostMapping
-    public String createProduct(@RequestBody ProductRequest request) {
+    public String createProduct(@Valid @RequestBody ProductRequest request) {
 
         CreateProductCommand createProductCommand = CreateProductCommand.builder()
                 .price(request.getPrice())
--- src/main/java/com/joao/studycase/productservice/controller/ProductRequest.java
+++ src/main/java/com/joao/studycase/productservice/controller/ProductRequest.java
@@ -3,13 +3,22 @@ package com.joao.studycase.productservice.controller;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
 @Data
 @Builder
 public class ProductRequest {
 
+    @NotBlank(message = "Title is a required field")
     private String title;
+
+    @Min(value = 1, message = "Price cannot be lower than 1")
     private BigDecimal price;
+
+    @Min(value = 1, message = "Quantity cannot be lower than 1")
+    @Max(value = 5, message = "Price cannot be greater than 1")
     private Integer quantity;
 }
